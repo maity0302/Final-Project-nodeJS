5464e3b29204310a0727cec92088c5383595dee5
MaiTy <ty53392@donga.edu.vn>
2022-07-06 13:23:28 +0700

    ejs -> hbs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-const usersRouter = require('./users');
 const siteRouter = require('./site');
 const universityRouter = require('./university');
 
 function route(app) {
   app.use('/university', universityRouter);
-  app.use('/users', usersRouter);
 
   app.use('/', siteRouter);
 }
